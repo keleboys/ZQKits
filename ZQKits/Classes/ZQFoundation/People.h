70d867b6090069264f80fa1dff2f7fd7ff4524c7
zq <13253467705@163.com>
2019-12-30 10:57:19 +0800

    no message

---
+++ ZQKits/Classes/ZQFoundation/People.h
@@ -0,0 +1,17 @@
+//
+//  People.h
+//  12345
+//
+//  Created by 张泉 on 2019/12/27.
+//  Copyright © 2019 张泉. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface People : NSString
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZQKits/Classes/ZQFoundation/People.m
@@ -0,0 +1,13 @@
+//
+//  People.m
+//  12345
+//
+//  Created by 张泉 on 2019/12/27.
+//  Copyright © 2019 张泉. All rights reserved.
+//
+
+#import "People.h"
+
+@implementation People
+
+@end
